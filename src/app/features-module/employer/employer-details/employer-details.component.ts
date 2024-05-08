67b373e3a9bc3cd1a6a61a14752a445d5d12718d
mohit.kumar@pib-insurance.com <mohit.kumar@pib-insurance.com>
2024-05-08 17:09:28 +0530

    Tenant and other Informatin added into app details

--- src/app/features-module/employer/employer-details/employer-details.component.ts
+++ src/app/features-module/employer/employer-details/employer-details.component.ts
@@ -29,136 +29,136 @@ export class EmployerDetailsComponent {
 
   onSubmit() { }
 
-  setForm() {
-    this.fields = [
-      {
-        fieldGroupClassName: 'row my-2',
-        fieldGroup: [
-          {
-            className: 'col-sm-3',
-            type: 'input',
-            key: 'applicantId',
-            props: {
-              label: 'Applicant ID',
-            },
-          },
-          {
-            template: `<button style:"display:flex;text-align:center;justify-content:center">Search</button>`,
-            className: 'col-sm-1 btn btn-outline-primary btn-sm',
-          },
-          {
-            className: 'col-sm-2',
-            key: 'status',
-            type: 'select',
-            props: {
-              label: 'Status',
-              options: [
-                { value: 1, text: 'Completed' },
-                { value: 2, text: 'Ongoing' },
-                { value: 3, text: 'Planning' },
-              ],
-              primitive: true,
-              required: true,
-            },
-          },
-          {
-            className: 'col-sm-3',
-            key: 'startDate',
-            type: 'date',
-            props: {
-              label: 'Start Date',
-              primitive: true,
-              required: true,
-              format: 'dd/MM/yyyy',
-            },
-          },
-          {
-            className: 'col-sm-3',
-            key: 'endDate',
-            type: 'date',
-            props: {
-              label: 'End Date',
-              primitive: true,
-              required: true,
-              format: 'dd/MM/yyyy',
-            },
-          },
-        ],
-      },
-      {
-        fieldGroupClassName: 'row',
-        fieldGroup: [
-          {
-            className: 'col-sm-4',
-            type: 'input',
-            key: 'globalSearch',
-            props: {
-              label: 'Global Search',
-            },
-          },
-          {
-            template: `<button style:"display:flex;text-align:center;justify-content:center">Search</button>`,
-            className: 'col-sm-1 btn btn-outline-primary btn-sm',
-          },
-          {
-            className: 'col-sm-3',
-            key: 'agentType',
-            type: 'select',
-            props: {
-              label: 'Agent Type',
-              options: [
-                { value: 1, text: 'Type 1' },
-                { value: 2, text: 'Type 2' },
-              ],
-              primitive: true,
-              required: true,
-            },
-          },
-          {
-            className: 'col-sm-4',
-            type: 'input',
-            key: 'agentName',
-            props: {
-              label: 'Agent Name',
-            },
-          },
-        ],
-      },
-      {
-        fieldGroupClassName: 'row',
-        fieldGroup: [
-          {
-            className: 'col-sm-3',
-            key: 'highRiskTaskId',
-            type: 'select',
-            props: {
-              label: 'High risk task',
-              options: [
-                { value: 1, text: 'Yes' },
-                { value: 2, text: 'No' },
-              ],
-              primitive: true,
-              required: true,
-            },
-          },
-          {
-            className: 'col-sm-3',
-            key: 'validationTask',
-            type: 'select',
-            props: {
-              label: 'Validation task',
-              options: [
-                { value: 1, text: 'Yes' },
-                { value: 2, text: 'No' },
-              ],
-              primitive: true,
-              required: true,
-            },
-          },
-        ]
-      }
-    ];
-  }
+  // setForm() {
+  //   this.fields = [
+  //     {
+  //       fieldGroupClassName: 'row my-2',
+  //       fieldGroup: [
+  //         {
+  //           className: 'col-sm-3',
+  //           type: 'input',
+  //           key: 'applicantId',
+  //           props: {
+  //             label: 'Applicant ID',
+  //           },
+  //         },
+  //         {
+  //           template: `<button style:"display:flex;text-align:center;justify-content:center">Search</button>`,
+  //           className: 'col-sm-1 btn btn-outline-primary btn-sm',
+  //         },
+  //         {
+  //           className: 'col-sm-2',
+  //           key: 'status',
+  //           type: 'select',
+  //           props: {
+  //             label: 'Status',
+  //             options: [
+  //               { value: 1, text: 'Completed' },
+  //               { value: 2, text: 'Ongoing' },
+  //               { value: 3, text: 'Planning' },
+  //             ],
+  //             primitive: true,
+  //             required: true,
+  //           },
+  //         },
+  //         {
+  //           className: 'col-sm-3',
+  //           key: 'startDate',
+  //           type: 'date',
+  //           props: {
+  //             label: 'Start Date',
+  //             primitive: true,
+  //             required: true,
+  //             format: 'dd/MM/yyyy',
+  //           },
+  //         },
+  //         {
+  //           className: 'col-sm-3',
+  //           key: 'endDate',
+  //           type: 'date',
+  //           props: {
+  //             label: 'End Date',
+  //             primitive: true,
+  //             required: true,
+  //             format: 'dd/MM/yyyy',
+  //           },
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       fieldGroupClassName: 'row',
+  //       fieldGroup: [
+  //         {
+  //           className: 'col-sm-4',
+  //           type: 'input',
+  //           key: 'globalSearch',
+  //           props: {
+  //             label: 'Global Search',
+  //           },
+  //         },
+  //         {
+  //           template: `<button style:"display:flex;text-align:center;justify-content:center">Search</button>`,
+  //           className: 'col-sm-1 btn btn-outline-primary btn-sm',
+  //         },
+  //         {
+  //           className: 'col-sm-3',
+  //           key: 'agentType',
+  //           type: 'select',
+  //           props: {
+  //             label: 'Agent Type',
+  //             options: [
+  //               { value: 1, text: 'Type 1' },
+  //               { value: 2, text: 'Type 2' },
+  //             ],
+  //             primitive: true,
+  //             required: true,
+  //           },
+  //         },
+  //         {
+  //           className: 'col-sm-4',
+  //           type: 'input',
+  //           key: 'agentName',
+  //           props: {
+  //             label: 'Agent Name',
+  //           },
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       fieldGroupClassName: 'row',
+  //       fieldGroup: [
+  //         {
+  //           className: 'col-sm-3',
+  //           key: 'highRiskTaskId',
+  //           type: 'select',
+  //           props: {
+  //             label: 'High risk task',
+  //             options: [
+  //               { value: 1, text: 'Yes' },
+  //               { value: 2, text: 'No' },
+  //             ],
+  //             primitive: true,
+  //             required: true,
+  //           },
+  //         },
+  //         {
+  //           className: 'col-sm-3',
+  //           key: 'validationTask',
+  //           type: 'select',
+  //           props: {
+  //             label: 'Validation task',
+  //             options: [
+  //               { value: 1, text: 'Yes' },
+  //               { value: 2, text: 'No' },
+  //             ],
+  //             primitive: true,
+  //             required: true,
+  //           },
+  //         },
+  //       ]
+  //     }
+  //   ];
+  // }
 
   setCompany() {
     this.companyFields = [
@@ -324,8 +324,10 @@ export class EmployerDetailsComponent {
             props: {
               label: 'Contact Type',
               options: [
-                { value: 1, text: 'test 1' },
-                { value: 2, text: 'test 2' },
+                { value: 1, text: 'Email' },
+                { value: 2, text: 'Phone' },
+                { value: 3, text: 'Fax' },
+                { value: 4, text: 'Post' },
               ],
 
             },
@@ -337,8 +339,10 @@ export class EmployerDetailsComponent {
             props: {
               label: 'Preference Type',
               options: [
-                { value: 1, text: 'test 1' },
-                { value: 2, text: 'test 2' },
+                { value: 1, text: 'Email' },
+                { value: 2, text: 'Phone' },
+                { value: 3, text: 'Fax' },
+                { value: 4, text: 'Post' },
               ],
 
             },
